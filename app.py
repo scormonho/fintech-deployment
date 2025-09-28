ec3fb6b5a6ebaabcacff710e1c446d8adfde6bca
scormon-predata-ai <scormon-predata-ai@gmail.com>
2025-09-28 16:10:25 +0800

    modified greet()

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import gradio as gr
 
 def greet(name):
-    return "Hello " + name + "!"
+    return "Hello " + name + ", I'm Scormon!"
 
 demo = gr.Interface(fn=greet, inputs="textbox", outputs="textbox")
 
